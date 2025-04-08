7d54cdea2683e8305d7a48b65f9c2537acda5158
ArwaElsherif <arwa01273648207@gmail.com>
2025-04-08 21:59:49 +0200

    finish textField

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -7,7 +7,24 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search City"),
+        backgroundColor: Colors.blueAccent,
+        title: Text('Search City', style: TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 30,horizontal: 8),
+              labelText: 'Search',
+              suffixIcon: Icon(Icons.search),
+              hintText: 'Enter city name',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.red ,width: 2 ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
