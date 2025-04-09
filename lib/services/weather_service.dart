9bd4c2ffb160c068f310b505b93ff99f09fcd7e3
ArwaElsherif <arwa01273648207@gmail.com>
2025-04-09 16:27:08 +0200

    created current method

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,3 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/weather_model.dart';
+
 class WeatherService {
-  
+  final Dio dio;
+  WeatherService({required this.dio});
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    Response response = await dio.get(
+      'http://api.weatherapi.com/v1/forecast.json?key=aa33b7ccdd884756a6f122754241009&q=$cityName&days=1',
+    );
+
+    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    return weatherModel;
+  }
 }
