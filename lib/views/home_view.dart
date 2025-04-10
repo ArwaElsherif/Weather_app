c077b7e64e6bd5cc4eca4e69acfea7b5e6a94ca9
ArwaElsherif <arwa01273648207@gmail.com>
2025-04-11 01:28:56 +0200

    finished cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_state.dart';
 import 'package:weather_app/views/search_view.dart';
+import 'package:weather_app/widgets/no_weather_body.dart';
 import 'package:weather_app/widgets/weather_info_body.dart';
 
 class HomeView extends StatefulWidget {
@@ -8,11 +12,9 @@ class HomeView extends StatefulWidget {
   @override
   State<HomeView> createState() => _HomeViewState();
 }
-
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
@@ -32,7 +34,19 @@ class _HomeViewState extends State<HomeView> {
           ),
         ],
       ),
-      body: WeatherInfoBody(),
+      body: BlocBuilder<GetWeatherCubit, WeatherState>(
+        builder: (context, state) {
+          if (state is NoWeatherState) {
+          return NoWeatherBody();
+          }
+          else if(state is WeatherLoadedState){
+            return WeatherInfoBody();
+          }
+          else{
+            return Text('oops there was an error');
+          }
+        },
+      ),
       );
   }
 }
--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -15,6 +17,7 @@ class SearchView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 18),
           child: TextField(
             onSubmitted: (value)async{
+              BlocProvider.of<GetWeatherCubit>(context).getWeather(cityName: value);
             Navigator.pop(context);
             },
             decoration: InputDecoration(
